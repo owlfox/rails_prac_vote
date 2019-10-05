5456639310dafb3432d01f6b136901bcbf263a0f
Michael, Ze-Ren Chen <tsejen.tw@gmail.com>
2019-10-05 13:03:48 +0800

    add instances error on creating candidate.. ForbiddenAttributesError

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -3,6 +3,17 @@ class CandidatesController < ApplicationController
     end
 
     def create
+        @candidate = Candidate.new(params[:candidate])
+
+        if @candidate.save
+            redirect_to '/candidates'
+        else
+            
+        end
+    end
+
+    def new
+        @candidate = Candidate.new
     end
 end
 
