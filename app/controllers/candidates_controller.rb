5d2b9ee75d721496250013d9ee39d209f20d96b1
Michael, Ze-Ren Chen <tsejen.tw@gmail.com>
2019-10-05 17:47:01 +0800

    Add list of candidates, use the best practices of link

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,19 +1,31 @@
 class CandidatesController < ApplicationController 
     def index
-    end
-
-    def create
-        @candidate = Candidate.new(params[:candidate])
-
-        if @candidate.save
-            redirect_to '/candidates'
-        else
-            
-        end
+        @candidates = Candidate.all
     end
 
     def new
         @candidate = Candidate.new
     end
+    
+    def create
+        @candidate = Candidate.new(candidate_params)
+
+        if @candidate.save
+            
+            flash[:notice] = "candidate added"
+            redirect_to '/candidates'
+        else
+            # use the same instance to render the page again
+
+            render :new
+        end
+    end
+
+    private
+    def candidate_params
+      params.require(:candidate).permit(:name, :age, :party, :politics)
+    end
+
+    
 end
 
