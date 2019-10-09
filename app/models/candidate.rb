bcba1d6ef84c640363eee8c9d8ce2bf9bb8549fb
Michael, Ze-Ren Chen <tsejen.tw@gmail.com>
2019-10-09 10:21:24 +0800

    examples of query scope

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,4 +1,13 @@
 class Candidate < ApplicationRecord
     validates :name, presence: true
     has_many :vote_logs
+
+    scope :young_people, -> {where('age < 35')}
+    scope :older_than, -> (x) {unscope(:where).where("age > #{x}")}
+
+    default_scope {where('age > 0')}
+
+    def self.available
+        where(is_available: true)
+    end
 end
